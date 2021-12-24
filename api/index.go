e22a18a0d98cbff132a521ef42e1c2dea911f2c0
lucasew <lucas59356@gmail.com>
2021-12-24 19:57:04 -0300

    genesis
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ api/index.go
@@ -0,0 +1,56 @@
+package handler
+
+import (
+	"bytes"
+	"net/http"
+	"net/url"
+	"html/template"
+	"time"
+
+	"github.com/go-shiori/go-readability"
+	"golang.org/x/net/context"
+	"golang.org/x/net/html"
+)
+
+const Template = `
+`
+var DefaultTemplate *template.Template
+
+func init() {
+    DefaultTemplate = template.Must(template.New("article").Parse(Template))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+    link, err := url.Parse(r.URL.Query().Get("url"))
+    if err != nil {
+        w.WriteHeader(400)
+        return
+    }
+    ctx, cancel := context.WithTimeout(r.Context(), time.Second*5)
+    defer cancel()
+    buf := bytes.NewBuffer([]byte{})
+    req, err := http.NewRequestWithContext(ctx, "GET", link.String(), buf)
+    if err != nil {
+        w.WriteHeader(400)
+        return
+    }
+    res, err := http.DefaultClient.Do(req)
+    if err != nil {
+        w.WriteHeader(res.StatusCode)
+        return
+    }
+    node, err := html.Parse(res.Body)
+    if err != nil {
+        w.WriteHeader(500)
+    }
+    parser := readability.NewParser()
+    parser.Debug = true
+    article, err := parser.ParseDocument(node, link)
+    if err != nil {
+        w.WriteHeader(422) // Unprocessable entity
+    }
+    err = DefaultTemplate.Execute(w, article)
+    if err != nil {
+        w.WriteHeader(500)
+    }
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lucasew/readability-web
+
+go 1.16
+
+require (
+	github.com/go-shiori/go-readability v0.0.0-20210627123243-82cc33435520
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+)
