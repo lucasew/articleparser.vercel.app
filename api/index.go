85c1e6b76bad416a326a0843017637ed30bd4f06
lucasew <lucas59356@gmail.com>
2025-04-03 14:43:30 -0300

    that responsive line
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- api/index.go
+++ api/index.go
@@ -21,6 +21,7 @@ const Template = `
 <html>
 <head>
 	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link id="theme" rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css">
 </head>
 <body>
